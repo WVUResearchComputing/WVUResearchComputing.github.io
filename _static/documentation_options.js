1193b4cb3b94655f1463c2353fe304f109324e59
Guillermo Avendano-Franco <gufranco@mail.wvu.edu>
2025-03-15 16:07:12 -0400

    [gufranco] 2025-03-15 16:07:12.406575 - Updateing Spruce

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.10.11',
+    VERSION: '2025.03.15',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
